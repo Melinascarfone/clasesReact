f114b8ca265503b83cb20b7ef1f340ff5f33af28
Melina Scarfone <melinasolscarfone@gmail.com>
2021-08-31 22:46:32 -0300

    clase7

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,53 +1,6 @@
-// import React, { useEffect, useState } from 'react';
 import ItemList from './ItemList'
 import { useEffect } from "react"
 import {useState} from "react"
-
-// const Products = () => {
-
-//     return new Promise ((resolve, reject) => {
-
-//     setTimeout(() =>
-
-//         resolve([
-//             {id: 1, destacado: true, nombre:'Resaltador Filgo', categoria: 'Rosa pastel', precio: 90, stock: 3},
-//             {id: 2, destacado: false, nombre:'Resaltador Filgo', categoria: 'Amarillo fluo', precio: 80, stock: 4},
-//             {id: 3, destacado: true, nombre:'Cuaderno A4', categoria: 'Rayado', precio: 180, stock: 2},
-//             {id: 4, destacado: false, nombre:'Lapicera Bic', categoria: 'Azul', precio: 50, stock: 4},
-//             ]),
-//             2000
-//         )
-//     })
-// }
-
-// const ItemListContainer = ({text}) => {
-
-//     const [dataToShow, setDataToShow] = useState([]);
-
-//     useEffect(() => {
-//         Products().then((data) => {
-//             let aux = data.filter(producto => producto.destacado)
-//             setDataToShow(aux)
-//         })
-//     }, [])   
-
-//     return dataToShow.length === 0 ? (
-//         <>
-//                     <h2>Cargando...</h2>
-//         </> 
-//         ) : (
-//         <>
-//                 <div>    
-//                     <h3>{text}</h3>
-//                 </div>
-//                 <div>    
-//                     <ItemList products={dataToShow} />
-//                 </div>  
-//         </>
-//         );
-// }
-// export default ItemListContainer; 
-
 
 const ItemListContainer = () => {
 
